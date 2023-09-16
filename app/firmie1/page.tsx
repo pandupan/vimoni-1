64d2f3455bfeb31882bad264f3f61592e533d785
pandupan <pangestup60@gmail.com>
2023-09-16 12:00:38 +0700

    [ADD] - Init

---
+++ app/firmie1/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      firmie1
+    </div>
+  )
+}
+
+export default page
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        theme: {
+          primary :'#8E7E6E',
+          secondary: {
+            DEFAULT : '#8577A6',
+            light: '#BAA2F2'
+          },
+          tertiary :'BAA2F2',
+          text :{
+            DEFAULT : '#333333',
+            blue :'#0000FF',
+            green: '#008000'
+          }
+        }
+      }
     },
   },
   plugins: [],
